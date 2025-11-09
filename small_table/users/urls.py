1712f423ff5a3ee4d103dc085b4bdd2f3fb7bc51
ayala-dev <ayala89017@gmail.com>
2025-11-09 13:42:17 +0200

    perrmission

--- small_table/users/urls.py
+++ small_table/users/urls.py
@@ -3,8 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 
 router = DefaultRouter()
-router.register('users', UserViewSet)
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
